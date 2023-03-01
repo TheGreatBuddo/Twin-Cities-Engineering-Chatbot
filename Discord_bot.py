12de2ae8f144a77efc935a8fea0f7620ba32cb29
Samuel Day <sday@midco.net>
2023-02-28 18:06:25 -0600

    First Commit of rasa init and discord.py
    discord.py is just the beginning of framing the rasa chatbot to interact between discord and rasa.

---
+++ Discord_bot.py
@@ -0,0 +1,28 @@
+TOKEN = 'MTA3MTc4NzE0ODQxNzcxMjIwOQ.GlEmoy._t3rCg_2dmmP6P3oazFndjk3tYupdjYYq37-yA'
+GUILD = "TheGreatBuddo's server"
+
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+
+#Intents is a thing.
+a = 4
+print(a)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+client.run(TOKEN)
